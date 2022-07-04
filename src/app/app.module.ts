8d598b6a83644eaaff7b2f7483b4ddb277ae2a22
MadanMohanReddy <madan.patakota@hotmail.com>
2022-07-04 20:13:34 +0530

    FormChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent  } from './app.component';
+
+import  { FormsModule } from '@angular/forms';
+//  i am importing the formmodule class from the @angular forms package.
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
