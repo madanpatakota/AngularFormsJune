ab9e4e131a9146d419bb3d5c6c516fae12fdbf54
MadanMohanReddy <madan.patakota@hotmail.com>
2022-07-06 20:13:22 +0530

    Template Driven approch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularFormsJune';
+
+  TeamList:any = [
+    {
+      Id: 1,
+      Name: 'Lakshmi',
+    },
+    {
+      Id: 2,
+      Name: 'Arjun',
+    },
+    {
+      Id: 3,
+      Name: 'Madan',
+     
+    },
+    {
+      Id: 4,
+      Name: 'Rakshith',
+     
+    },
+    {
+      Id: 5,
+      Name: 'Barsa',
+    },
+  ];
+
+
+  Relavants = [{ ID: 1, Name: "Yes" }, { ID: 2, Name: "No" }];
+
+
+  @ViewChild("modForm")
+  _modForm: any;
+
+
+  evtSubmit(){
+    console.log(this._modForm)
+    ////******** this is highly recommndation....... */
+  }
+
+
+  evtSetValue(){
+    this._modForm.setValue({
+      UserEmail:"madan@gmail.com",
+      UserName : "Madan",
+      modDeveloper : 3,
+      MyCommentsGroup : {
+        modComments : "Test cases",
+        modReview : "Test Review"
+      }
+    })
+  }
+
+
+  evtPatchValue(){
+    this._modForm.form.patchValue({
+      UserEmail:"madan@gmail.com",
+      UserName : "Madan",
+    });
+  }
+
+  evtReset(){
+    this._modForm.reset();
+  }
+
+
+
+  
+
+
 }
